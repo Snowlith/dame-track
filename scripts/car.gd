77eb52858ea7e489f04be8666eb30f02242686b0
Snowlith <vilmtorm@gmail.com>
2025-03-22 17:39:45 -0500

    Added comments

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 class_name Car
 
+@export var jump_impulse: float = 500
 @export var engine_power: Curve
 @export var top_speed: float = 20
 @export var steering_angle: float = 20
@@ -15,7 +16,7 @@ func _ready() -> void:
 		wheel.add_collision_exception(self)
 
 func _physics_process(delta: float) -> void:
-	acceleration_input = Input.get_axis("down", "up")
+	acceleration_input = Input.get_axis("forward", "backward")
 	steering_input = Input.get_axis("right", "left")
 	
 	%FrontLeftWheel.rotation.y = steering_input * deg_to_rad(steering_angle)
@@ -24,7 +25,7 @@ func _physics_process(delta: float) -> void:
 		wheel.apply_forces(self, delta)
 	
 	if Input.is_action_just_pressed("jump"):
-		apply_central_impulse(Vector3.UP * 500)
+		apply_central_impulse(Vector3.UP * jump_impulse)
 	
 func _process(delta):
 	%SpeedBar.value = abs(linear_velocity.dot(global_basis.z))
--- scripts/wheel.gd
+++ scripts/wheel.gd
@@ -16,17 +16,22 @@ class_name Wheel
 @onready var mesh: Node3D = $MeshInstance3D
 
 var _previous_spring_length: float
-var _current_rotation_speed: float
+var _current_rotational_speed: float
 
 func _ready():
+	# Ensure that the ray cast is not a frame behind when applying forces
 	ray_cast.process_physics_priority = -100
-	_previous_spring_length = spring_resting_length
+	
+	# Reset the mesh position
 	mesh.position.y = -spring_resting_length + wheel_mesh_radius
+	
+	_previous_spring_length = spring_resting_length
 	
 func _process(delta):
 	mesh.position.y = lerp(mesh.position.y, -_previous_spring_length + wheel_mesh_radius, 50 * delta)
-	mesh.rotation.x += _current_rotation_speed * delta
+	mesh.rotation.x += _current_rotational_speed * delta
 
+# Called by the car to avoid wheel-car collisions
 func add_collision_exception(collision_object: CollisionObject3D):
 	ray_cast.add_exception(collision_object)
 
@@ -34,65 +39,68 @@ func apply_forces(car: Car, delta: float) -> void:
 	if not ray_cast.is_colliding():
 		_previous_spring_length = spring_resting_length
 		return
+	
 	var collision_point: Vector3 = ray_cast.get_collision_point()
 	
 	var suspension_force: Vector3 = _get_suspension_force(car, collision_point, delta)
-	var acceleration_force: Vector3 = _get_acceleration_force(car, collision_point)
-	var steering_force: Vector3 = _get_steering_force(car, collision_point, delta)
-	var friction_force: Vector3 = _get_friction_force(car, collision_point, delta, suspension_force)
+	var acceleration_force: Vector3 = _get_acceleration_force(car)
+	var steering_force: Vector3 = _get_steering_force(car, delta)
+	var friction_force: Vector3 = _get_friction_force(car, delta, suspension_force)
 	
+	if debug:
+		DebugDraw3D.draw_arrow(collision_point, collision_point + acceleration_force * 0.01, Color.BLUE, 0.05, true)
+		DebugDraw3D.draw_arrow(collision_point, collision_point + suspension_force * 0.005, Color.GREEN, 0.05, true)
+		DebugDraw3D.draw_arrow(collision_point, collision_point + steering_force * 0.005, Color.RED, 0.05, true)
+		DebugDraw3D.draw_arrow(collision_point, collision_point + friction_force * 0.05, Color.YELLOW, 0.05, true)
+		
 	var net_force: Vector3 = suspension_force + acceleration_force + steering_force + friction_force
-	
+	# Apply the net force at the wheel position relative to the car
 	car.apply_force(net_force, collision_point - car.global_position)
 
-func _get_acceleration_force(car: Car, collision_point: Vector3) -> Vector3:
+# Calculate the force the ray cast would apply if it were a damped spring
+func _get_suspension_force(car: Car, collision_point: Vector3, delta: float) -> Vector3:
+	var spring_direction: Vector3 = global_basis.y
+	
+	var spring_length: float = (collision_point - ray_cast.global_position).length()
+	
+	# Calculate the velocity of the spring which is important for damping
+	var spring_velocity: float = (spring_length - _previous_spring_length) / delta
+	
+	# Store the current spring length for the next frame
+	_previous_spring_length = spring_length
+
+	var spring_force: float = (1.0 - spring_length / spring_resting_length) * spring_strength
+	var damping_force: float = -spring_velocity * spring_damping
+
+	return spring_direction * (spring_force + damping_force)
+
+# Calculate acceleration or deceleration based on the player's input
+func _get_acceleration_force(car: Car) -> Vector3:
 	if is_front_wheel:
 		return Vector3.ZERO
 	
 	var acceleration_direction: Vector3 = global_basis.z
 	
+	# Get the current speed compared to the top speed
 	var car_speed: float = abs(car.linear_velocity.dot(car.global_basis.z))
 	var speed_normalized: float = clamp(car_speed / car.top_speed, 0, 1)
 	
-	var torque: float = car.acceleration_input * car.engine_power.sample(speed_normalized)
+	# Get the player's input and scale it by the engine power at the current speed
+	var torque: float = -car.acceleration_input * car.engine_power.sample(speed_normalized)
 	
-	var acceleration_force: Vector3 = acceleration_direction * torque
-	
-	if debug:
-		DebugDraw3D.draw_arrow(collision_point, collision_point + acceleration_force * 0.01, Color(0, 0, 1,), 0.05, true)
-	
-	return acceleration_force
+	return acceleration_direction * torque
 
-func _get_suspension_force(car: Car, collision_point: Vector3, delta: float) -> Vector3:
-	var spring_direction: Vector3 = global_basis.y
-	
-	var spring_length: float = (collision_point - ray_cast.global_position).length()
-
-	# Calculate spring velocity for damping
-	var spring_velocity: float = (spring_length - _previous_spring_length) / delta
-	
-	_previous_spring_length = spring_length
-
-	# Calculate and apply forces
-	var spring_force: float = (1.0 - spring_length / spring_resting_length) * spring_strength
-	var damping_force: float = -spring_velocity * spring_damping
-
-	var suspension_force: Vector3 = spring_direction * (spring_force + damping_force)
-	
-	#car.apply_force(suspension_force, collision_point - car.global_position)
-
-	if debug:
-		DebugDraw3D.draw_arrow(collision_point, collision_point + suspension_force * 0.005, Color(0, 1, 0,), 0.05, true)
-	
-	return suspension_force
-	
-func _get_steering_force(car: Car, collision_point: Vector3, delta: float) -> Vector3:
+# Calculate the force necessary to align the car's velocity to the wheels
+func _get_steering_force(car: Car, delta: float) -> Vector3:
 	var forward_direction: Vector3 = global_basis.z
 	var steering_direction: Vector3 = global_basis.x
 	var state := PhysicsServer3D.body_get_direct_state(car.get_rid())
 	var local_velocity := state.get_velocity_at_local_position(global_position - car.global_position)
-	_current_rotation_speed = local_velocity.dot(forward_direction) / wheel_mesh_radius
 	
+	# Save the rotational speed to spin the wheel in _process
+	_current_rotational_speed = local_velocity.dot(forward_direction) / wheel_mesh_radius
+	
+	# Sample the current grip from the grip curve if velocity is not negligible
 	var current_grip: float = 1.0
 	if not is_zero_approx(local_velocity.length()):
 		var alignment: float = abs(forward_direction.dot(local_velocity.normalized()))
@@ -100,31 +108,24 @@ func _get_steering_force(car: Car, collision_point: Vector3, delta: float) -> Ve
 	
 	var lateral_velocity: float = steering_direction.dot(local_velocity)
 	var desired_acceleration: float = -lateral_velocity * current_grip
-
-	var steering_force: Vector3 = steering_direction * steering_strength * desired_acceleration
-
-	if debug:
-		DebugDraw3D.draw_arrow(collision_point, collision_point + steering_force * 0.005, Color(1, 0, 0,), 0.05, true)
 	
-	return steering_force
-	
-func _get_friction_force(car: Car, collision_point: Vector3, delta: float, suspension_force: Vector3) -> Vector3:
+	return steering_direction * steering_strength * desired_acceleration
+
+# Calculate friction using the normal force obtained from the suspension force
+func _get_friction_force(car: Car, delta: float, suspension_force: Vector3) -> Vector3:
 	var forward_direction: Vector3 = global_basis.z
 	var state := PhysicsServer3D.body_get_direct_state(car.get_rid())
 	var local_velocity := state.get_velocity_at_local_position(global_position - car.global_position)
 	
 	var longitudinal_velocity: float = local_velocity.dot(forward_direction)
-	var friction_direction = forward_direction * -sign(longitudinal_velocity)
+	var friction_direction: Vector3 = forward_direction * -sign(longitudinal_velocity)
 	
 	var normal_force: float = suspension_force.length()
 	
-	var friction_force = friction_direction * friction_coefficient * normal_force
+	var friction_force: Vector3 = friction_direction * friction_coefficient * normal_force
 	
-	# Smoothing factor to avoid flickering at low velocity
+	# Diminish friction at low velocity to avoid flickering
 	if abs(longitudinal_velocity) < 0.1:
 		friction_force *= abs(longitudinal_velocity) * 10
-	
-	if debug:
-		DebugDraw3D.draw_arrow(collision_point, collision_point + friction_force * 0.05, Color.YELLOW, 0.05, true)
 	
 	return friction_force
